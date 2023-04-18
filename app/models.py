a2aea32cab78114b2cb3aab07a5bd9fdeb624425
Tuska101 <amtuska@gmail.com>
2023-04-18 18:26:55 +0300

    added posts

--- app/models.py
+++ app/models.py
@@ -2,6 +2,30 @@
 Definition of models.
 """
 
+from audioop import reverse
+from datetime import datetime
+from email.policy import default
+from tabnanny import verbose
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100, unique_for_date="posted", verbose_name="Заголовок")
+    description = models.TextField(verbose_name="Краткое содержание")
+    content = models.TextField(verbose_name="Полное содержание")
+    posted = models.DateTimeField(default=datetime.now(), db_index=True, verbose_name="Опубликована")
+
+    def get_absolute_url(self):
+        return reverse("blogpost", args=[str(self.id)])
+    def __str__(self):
+        return self.title
+    class Meta:
+        db_table = "Posts"
+        ordering = ["-posted"]
+        verbose_name = "статья блога"
+        verbose_name_plural = "статьи блога"
+
+admin.site.register(Blog)
